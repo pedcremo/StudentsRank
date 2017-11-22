203e24dc3cd2ab3d98ff3f1373ee5a59e51596a9
Pere Crespo <pedcremo@iestacio.com>
2017-11-22 17:39:42 +0100

    no localstorage

--- src/client/dataservice.js
+++ src/client/dataservice.js
@@ -7,35 +7,37 @@ import GradedTask from './classes/gradedtask.js';
 function updateFromServer() {
   if (context.user.id) {
     loadTemplate('api/getStudents',function(response) {
-                            localStorage.setItem('students',response);
-                            loadStudentsFromLocalStorage();
+                            //localStorage.setItem('students',response);
+                            loadStudents(response);
                             context.getTemplateRanking();
                           },'GET','',false);
     loadTemplate('api/getGradedTasks',function(response) {
-                          localStorage.setItem('gradedTasks',response);
-                          loadGradedTasksFromLocalStorage();
+                          //localStorage.setItem('gradedTasks',response);
+                          loadGradedTasks(response);
                           context.getTemplateRanking();
                         },'GET','',false);
   }
 }
 /** Save students in localstorage and in server side */
 function saveStudents(arrayStudents) {
-  localStorage.setItem('students',arrayStudents);
+  //localStorage.setItem('students',arrayStudents);
   loadTemplate('api/saveStudents',function(response) {
                           console.log('SAVE STUDENTS ' + response);
-                        },'POST',localStorage.getItem('students'),false);
+                        //},'POST',localStorage.getItem('students'),false);
+                      },'POST',arrayStudents,false);
+
 }
 /** Save grades in localstorage and in server side */
 function saveGradedTasks(arrayGT) {
-  localStorage.setItem('gradedTasks',arrayGT);
+  //localStorage.setItem('gradedTasks',arrayGT);
   loadTemplate('api/saveGradedTasks',function(response) {
                           console.log('SAVE GRADED TASKS ' + response);
-                        },'POST',localStorage.getItem('gradedTasks'),false);
+                        },'POST',arrayGT,false);
 }
-/** Load students from localStorage and map to Person instances in context */
-function loadStudentsFromLocalStorage() {
-  if (localStorage.getItem('students')) {
-    let students_ = new Map(JSON.parse(localStorage.getItem('students')));
+/** Load students from AJAX response and map to Person instances in context */
+function loadStudents(studentsStr) {
+  //if (localStorage.getItem('students')) {
+    let students_ = new Map(JSON.parse(studentsStr));
     students_.forEach(function(value_,key_,students_) {
         students_.set(key_,new Person(value_.name,value_.surname,
           value_.attitudeTasks,value_.id));
@@ -43,17 +45,17 @@ function loadStudentsFromLocalStorage() {
     context.students = students_;
   }
 
-}
-/** Load graded tasks from localStorage and map to GradedTasks instances in context */
-function loadGradedTasksFromLocalStorage() {
-  if (localStorage.getItem('gradedTasks')) {
-    let gradedTasks_ = new Map(JSON.parse(localStorage.getItem('gradedTasks')));
+//}
+/** Load graded tasks from AJAX response and map to GradedTasks instances in context */
+function loadGradedTasks(gradedTasksStr) {
+  //if (localStorage.getItem('gradedTasks')) {
+    let gradedTasks_ = new Map(JSON.parse(gradedTasksStr));
     gradedTasks_.forEach(function(value_,key_,gradedTasks_) {
         gradedTasks_.set(key_,new GradedTask(value_.name,value_.description,value_.weight,
           value_.studentsMark,value_.id));
       });
     context.gradedTasks = gradedTasks_;
-  }
+  //}
 }
 
 export {updateFromServer,saveStudents,saveGradedTasks};
