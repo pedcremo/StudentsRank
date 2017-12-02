fdf023d48bc668a04c95ff2aab1926ed24c3a9b5
nicklaus_ <pedcremo@gmail.com>
2017-12-02 09:46:20 +0100

    deploy script fix

--- src/server/scripts/deploy.sh
+++ src/server/scripts/deploy.sh
@@ -3,6 +3,5 @@ cd /home/pedcremo/StudentsRank
 git checkout experimental
 git pull origin experimental
 npm install
-npm stop
-npm start
-node_modules/forever/bin/forever start src/server/hook.js
+npm restart
+#node_modules/forever/bin/forever start src/server/hook.js
