52dd98cb090f7fa481752e60e1cd731474875856
Zar21 <ivanperezfita@gmail.com>
2018-01-08 19:21:08 +0100

    Some protractor

--- tests_e2e/conf.js
+++ tests_e2e/conf.js
@@ -1,6 +1,6 @@
 exports.config = {
     framework: 'jasmine',
-    seleniumAddress: 'http://localhost:4444/wd/hub',
+    //seleniumAddress: 'http://localhost:4444/wd/hub',
     baseUrl: 'http://localhost:8000/',
     specs: ['login.js'],
     capabilities: {
--- tests_e2e/login.js
+++ tests_e2e/login.js
@@ -3,6 +3,7 @@ describe('Login app', () => {
     beforeEach(function() {
         browser.ignoreSynchronization = true;
         browser.get('/');
+        browser.sleep(2000);
         username = element(by.name('username'));
         password = element(by.name('password'));
         loginbutton = element(by.buttonText('Login'));
@@ -12,9 +13,22 @@ describe('Login app', () => {
         password.sendKeys('admin');
         loginbutton.click().then(function() {
                 browser.sleep(2000);
-                var tableRanking = element(by.id('idTableRankingBody'));
-                expect(tableRanking.isDisplayed()).toBeTruthy();
-                let profileImages = element(by.className('profile')); //Image profiles TEST
+                var addStudentLink = $('[href*="#addStudent"]');
+                //var tableRanking = element(by.id('idTableRankingBody'));
+                expect(addStudentLink.isDisplayed()).toBeTruthy();
+                //let profileImages = element(by.className('profile')); //Image profiles TEST
+                addStudentLink.click();
+                browser.sleep(2000);
+                username = element(by.name('firstname'));
+                var surname = element(by.name('surnames'));
+                username.sendKeys('Paco');
+                surname.sendKeys('El Maco');
+                var savebutton = element(by.buttonText('Save'));
+                savebutton.click();
+                browser.sleep(4000);
+                var deleteStudentLink = $('[href*="##deleteStudent/-1935365642"]');
+                deleteStudentLink.click();
+                browser.sleep(2000);
               });
       });
   });
