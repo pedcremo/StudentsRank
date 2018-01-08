13ef53a884b354925ea29c3cd9253160f3c3c9bb
nicklaus_ <pedcremo@gmail.com>
2018-01-08 21:57:40 +0100

    some more protractor

--- tests_e2e/login.js
+++ tests_e2e/login.js
@@ -3,7 +3,7 @@ describe('Login app', () => {
     beforeEach(function() {
         browser.ignoreSynchronization = true;
         browser.get('/');
-        browser.sleep(2000);
+        browser.sleep(4000);
         username = element(by.name('username'));
         password = element(by.name('password'));
         loginbutton = element(by.buttonText('Login'));
@@ -12,23 +12,65 @@ describe('Login app', () => {
         username.sendKeys('admin');
         password.sendKeys('admin');
         loginbutton.click().then(function() {
-                browser.sleep(2000);
-                var addStudentLink = $('[href*="#addStudent"]');
-                //var tableRanking = element(by.id('idTableRankingBody'));
-                expect(addStudentLink.isDisplayed()).toBeTruthy();
-                //let profileImages = element(by.className('profile')); //Image profiles TEST
-                addStudentLink.click();
-                browser.sleep(2000);
-                username = element(by.name('firstname'));
-                var surname = element(by.name('surnames'));
-                username.sendKeys('Paco');
-                surname.sendKeys('El Maco');
-                var savebutton = element(by.buttonText('Save'));
-                savebutton.click();
-                browser.sleep(4000);
-                var deleteStudentLink = $('[href*="##deleteStudent/-1935365642"]');
+                browser.sleep(2000);           
+                
+                element(by.id('subjectName')).isPresent().then(function(result) {
+                    if ( result ) {
+                        //Whatever if it is true (displayed)
+                        console.log('JJJJJJJJJJ');
+                        var subjectname = element(by.id('subjectName'));
+                        subjectname.sendKeys('prova');
+                        var newSubject = element(by.id('newSubject'));
+                        newSubject.click().then(function(){
+                            browser.sleep(2000);
+                            console.log('CLICK CLICK CLICK');
+                        });
+                    }else{
+                        addStudent('Paco','ElMaco');
+                        deleteStudent('480131812');
+                    }
+                });
+                /*addStudent('Paco','El Maco');
+                var deleteStudentLink = $('[href*="#deleteStudent/1957027836"]');
                 deleteStudentLink.click();
                 browser.sleep(2000);
+                browser.switchTo().alert().then(function (alert) {
+                    
+                            alert.accept();
+                            return browser.get('/');
+                    
+                });
+                browser.sleep(2000);*/
+                
               });
       });
   });
+
+  function addStudent(name_,surname_) {
+    var addStudentLink = $('[href*="#addStudent"]');
+    //var tableRanking = element(by.id('idTableRankingBody'));
+    expect(addStudentLink.isDisplayed()).toBeTruthy();
+    //let profileImages = element(by.className('profile')); //Image profiles TEST
+    addStudentLink.click();
+    browser.sleep(2000);
+    username = element(by.name('firstname'));
+    var surname = element(by.name('surnames'));
+    username.sendKeys(name_);
+    surname.sendKeys(surname_);
+    var savebutton = element(by.buttonText('Save'));
+    savebutton.click();
+    browser.sleep(4000);    
+  }
+
+  function deleteStudent(id) {
+    var deleteStudentLink = $('[href*="#deleteStudent/'+ id +'"]');
+    deleteStudentLink.click();
+    browser.sleep(2000);
+    browser.switchTo().alert().then(function (alert) {
+        
+                alert.accept();
+                return browser.get('/');
+        
+    });
+    browser.sleep(2000);
+  }
